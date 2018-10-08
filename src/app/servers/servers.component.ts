707f0ed77d5dc21b5f003c59f6df0c73394da81e
Dell\Mabel <robert07ravikumar@gmail.com>
2018-10-08 11:50:26 +0400

    String interpolation and property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: `<app-server></app-server>
-  <app-server></app-server>`,
+  //selector: '[app-servers]',
+  selector: '.app-servers',
+  //template: `<app-server></app-server>
+  //<app-server></app-server>`,
+  templateUrl : './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  isNewServerAdditionEnabled = false;
+
+  constructor() { 
+    setTimeout(() => {
+      this.isNewServerAdditionEnabled = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
